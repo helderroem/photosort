cbad46ab301e868592cfb7d1874f04d591da4a3e
robinmarr-ft <robin.marr@ft.com>
2018-08-10 11:43:29 +0100

    extract some constants

---
+++ constants.js
@@ -0,0 +1,17 @@
+const { join } = require('path')
+
+export const basePath = join(__dirname, 'samples') // Join('Volumes', 'Masters', 'Pictures');
+export const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+]
