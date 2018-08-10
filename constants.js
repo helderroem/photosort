d8d957beb8e3c67a3fce0d5975a9469969e390d0
robinmarr-ft <robin.marr@ft.com>
2018-08-10 15:01:09 +0100

    remove constants, don't need them anymore

--- constants.js
+++
@@ -1,4 +0,0 @@
-const { join } = require('path')
-
-export const basePath = join(__dirname, 'samples') // Join('Volumes', 'Masters', 'Pictures');
-export const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
