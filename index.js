8ad253676cdc3ba976a2202965a187c4bee0d7f7
robinmarr-ft <robin.marr@ft.com>
2018-08-11 07:47:06 +0100

    tag with camera name instead of a color, add a progress bar as output

--- index.js
+++ index.js
@@ -2,50 +2,37 @@ import { resolve, relative, join } from 'path'
 import fg from 'fast-glob'
 import curry from 'lodash.curry'
 import { copy as move } from 'fs-extra'
-import { yellow, blue } from 'chalk'
-import { addColor } from './xattr'
-
+import { yellow, blue, green, white } from 'chalk'
+import addTags from './xattr'
 import readExif from './exif'
+import ProgressBar from 'progress'
 
-const deriveNewFolder = (writePath, DateTimeOriginal) => (
-  join(writePath, DateTimeOriginal.getFullYear().toString(), DateTimeOriginal.toLocaleString('en-GB', { month: 'long' }))
+const deriveNewFolder = (writePath, date) => (
+  join(writePath, date.getFullYear().toString(), date.toLocaleString('en-GB', { month: 'long' }))
 )
 
 const deriveNewFilename = (readPath, imagePath) => relative(readPath, imagePath).replace(/\//g, '_')
 
-const createNewLocation = (readPath, writePath, imagePath, date) => (
-  resolve(deriveNewFolder(writePath, date), deriveNewFilename(readPath, imagePath))
-)
-
 const readPath = './samples/'
 const writePath = 'Pictures/'
-const modelColorMap = {
-  'Canon DIGITAL IXUS 70': 'Blue',
-  'Canon EOS 400D DIGITAL': 'Green',
-  'Canon EOS 760D': 'Purple',
-  'DMC-TZ60': 'Orange',
-  'Canon PowerShot S40': 'Yellow'
-}
-
 const processFile = curry((readPath, writePath, imagePath) => readExif(imagePath)
   .then(({ Model, DateTimeOriginal }) => {
-    const newPath = createNewLocation(readPath, writePath, imagePath, DateTimeOriginal)
-    console.log(yellow(`moving ${blue(relative(readPath, imagePath))} to ${blue(relative(writePath, newPath))} and tagging ${Model} ${modelColorMap[Model]}`))
+    const newPath = resolve(deriveNewFolder(writePath, DateTimeOriginal), deriveNewFilename(readPath, imagePath))
     return move(imagePath, newPath)
-      .then(addColor(newPath, modelColorMap[Model]))
+			.then(() => addTags(newPath, Model))
   })
-  .catch(console.error)
 )
 
-console.log(`searching ${readPath} for photos`)
+console.log(white(`searching ${blue(readPath)} for image(s)`))
 const globPatterns = ['**/*.{CR2,jpg,jpeg,dng,JPG,JPEG,DNG,RW2}']
 const globOptions = { cwd: readPath, absolute: true }
 
 fg(globPatterns, globOptions)
   .then(files => {
-    console.log(`processing ${files.length}`)
-    return files
+		const total = files.length;
+    console.log(white(`processing ${blue(total)} image(s)`))
+    return [files, new ProgressBar(`${yellow('[:bar]')} ${blue(':current/:total')}`, { total })]
   })
-  .then(files => Promise.all(files.map(processFile(readPath, writePath))))
-  .then(() => console.log('done 👍'))
+  .then(([ files, progress ]) => Promise.all(files.map(file => processFile(readPath, writePath, file).then(progress.tick.bind(progress)))))
+  .then(() => console.log(green('done 👍')))
   .catch(console.error)
