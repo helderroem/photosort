75781ee57229cc4e5f3808beefac07ecdb0209fd
robinmarr-ft <robin.marr@ft.com>
2018-08-13 17:14:21 +0100

    turn into a cli with yargs

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { yellow, blue, green, white } from 'chalk'
 import curry from 'lodash.curry'
 import ProgressBar from 'progress'
-import { processFiles } from './processing'
+import yargs from 'yargs'
+
+import processFiles from './processing'
 
 const progress = (total) => {
   console.log(white(`processing ${blue(total)} image(s)`))
@@ -12,13 +14,29 @@ const progressErrorHandler = curry((progress, error) => {
   progress.tick()
 })
 
-export default (readPath, writePath, globPatterns = ['**/*.{CR2,jpg,jpeg,dng,JPG,JPEG,DNG,RW2,tiff}']) => {
-  console.log(white(`searching ${blue(readPath)} for image(s)`))
+export default () => {
+  const { inputDir, outputDir, glob } = yargs.usage('$0 <outputDir>', 'sort files by year and month using exif data', yargs => {
+    yargs.positional('outputDir', {
+      describe: 'the directory to output sorted files',
+      type: 'string'
+    })
+      .option('i', {
+        alias: 'inputDir',
+        describe: 'the input directory',
+        default: process.cwd()
+      })
+      .option('g', {
+        alias: 'glob',
+        describe: 'glob pattern(s) to search',
+        default: '**/*.{CR2,jpg,jpeg,dng,JPG,JPEG,DNG,RW2,tiff}'
+      })
+  }).argv
+  console.log(white(`searching ${blue(inputDir)} for image(s)`))
   return processFiles(
     progress,
     progressErrorHandler,
-    readPath,
-    writePath,
-    globPatterns
+    inputDir,
+    outputDir,
+    glob
   ).then(() => console.log(green('done 👍'))).catch(console.error)
 }
